7feafd67603a3f1dba9e6caf8bbfeee2a1d3f28a
neerajng <neerajnrjng@gmail.com>
2023-06-24 10:19:39 +0530

    settings changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import React from 'react';
+import MyComponent from './components/Component1/MyComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <MyComponent/>
       </header>
     </div>
   );
